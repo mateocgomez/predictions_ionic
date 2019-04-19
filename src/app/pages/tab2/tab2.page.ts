c0aa89caacd51c96b909fc54682ad1d78114dd82
mateocgomez <gomezmateo94@gmail.com>
2019-04-19 09:05:12 -0500

    Cambios en sockets

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -7,16 +7,14 @@ import { TwitterSerivice } from '../../services/twitter.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  user: Array<string> = [];
-
-  
+  user: Array<string> = ['@UCatolicaCol', 'Esta es la base del conocimiento para otros proyectos de grado más.'];
+  president: Array<string> = [''];
 
   constructor(private messageTwitter: TwitterSerivice){}
   ngOnInit(): void {
     this.messageTwitter.messages.subscribe(msg=>{
       this.user.unshift(msg.user, msg.message );
-     
-
+      console.log(msg.calculate['@IvanDuque'].positivo);
     });
   }
 
